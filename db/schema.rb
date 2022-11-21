30b527351a6d18142a840afb4a434fead62f9110
Emmanuel Landron <emmanuel@aventure.bio>
2022-11-21 15:03:32 +0100

    vehicle model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_135843) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_140235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,4 +29,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_135843) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vehicles", force: :cascade do |t|
+    t.string "type"
+    t.string "details"
+    t.string "name"
+    t.string "street"
+    t.string "city"
+    t.string "zipcode"
+    t.integer "price"
+    t.integer "street_number"
+    t.boolean "available"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
+  end
+
+  add_foreign_key "vehicles", "users"
 end
