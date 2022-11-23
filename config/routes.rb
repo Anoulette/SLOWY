7e2ee1c844c9560a6e68259038b7f898480a6826
gaelle ozanon <ozanongaelle@live.fr>
2022-11-23 13:25:52 +0100

    Edit bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "vehicles#index"
   devise_for :users
   get '/dashboard', to: "bookings#dashboard"
+  get '/dashboard/booking/:id/edit', to: "bookings#edit", as: :edit_dashboard
+  patch '/dashboard/bookings/:id', to: "bookings#update"
 
   resources :vehicles do
     resources :bookings, except: :destroy
