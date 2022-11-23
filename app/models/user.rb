f463a24ba3a39596eb9e1f9753549e224756d8f7
Emmanuel Landron <emmanuel@aventure.bio>
2022-11-23 14:07:28 +0100

    User avatar mgmt

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings, dependent: :destroy
   has_many :vehicles, dependent: :destroy
+  has_one_attached :avatar
 
   validates :first_name, presence: true
   validates :last_name, presence: true
